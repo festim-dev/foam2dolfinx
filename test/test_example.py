4e416fd536bb38a0bf429829084b80060532329d
jhdark <darkj385@mit.edu>
2025-03-07 11:56:55 -0500

    remove test with mixed topology mesh

--- test/test_example.py
+++ test/test_example.py
@@ -8,10 +8,3 @@ def test_reading_and_writing_cavity_example():
     vel = my_of_reader.create_dolfinx_function(t=2.5)
 
     assert isinstance(vel, dolfinx.fem.Function)
-
-
-def test_reading_example_2():
-    my_of_reader = OpenFOAMReader(filename=examples.download_openfoam_tubes(load=False))
-    vel = my_of_reader.create_dolfinx_function(t=1000.0)
-
-    assert isinstance(vel, dolfinx.fem.Function)
