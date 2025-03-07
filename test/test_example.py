d54cbd6332f5e958c98eb9f292f739fc0a525949
jhdark <darkj385@mit.edu>
2025-03-07 11:50:39 -0500

    just read files in test

--- test/test_example.py
+++ test/test_example.py
@@ -1,13 +1,17 @@
 from foam2dolfinx import OpenFOAMReader
-from dolfinx.io import VTXWriter
+import dolfinx
 from pyvista import examples
-from mpi4py import MPI
 
 
-def test_reading_and_writing_cavity_example(tmpdir):
+def test_reading_and_writing_cavity_example():
     my_of_reader = OpenFOAMReader(filename=examples.download_cavity(load=False))
     vel = my_of_reader.create_dolfinx_function(t=2.5)
 
-    output_filename = str(tmpdir.join("velocity_points_alt_test.bp"))
-    writer = VTXWriter(MPI.COMM_WORLD, output_filename, vel, engine="BP5")
-    writer.write(t=0)
+    assert isinstance(vel, dolfinx.fem.Function)
+
+
+def test_reading_example_2():
+    my_of_reader = OpenFOAMReader(filename=examples.download_openfoam_tubes(load=False))
+    vel = my_of_reader.create_dolfinx_function(t=1000.0)
+
+    assert isinstance(vel, dolfinx.fem.Function)
