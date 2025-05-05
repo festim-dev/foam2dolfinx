9cd8f863c88dbfbb2dfd5d1d12a1ca88be202556
jhdark <darkj385@mit.edu>
2025-05-05 17:03:33 -0400

    additional test and test data

--- test/test_read_with_pyvista.py
+++ test/test_read_with_pyvista.py
@@ -1,3 +1,6 @@
+import zipfile
+from pathlib import Path
+
 import pytest
 from pyvista import examples
 
@@ -23,3 +26,24 @@ def test_error_rasied_when_cells_wanted_are_not_in_file_provided():
         match=r"No cell type 1 found in the mesh\. Found \[.*12]",
     ):
         my_reader._read_with_pyvista(t=0)
+
+
+def test_error_rasied_when_subdomain_is_not_given_in_multidomain_case(tmpdir):
+    zip_path = Path("test/data/test_2Regions.zip")
+    extract_path = Path(tmpdir) / "test_2Regions"
+
+    # Unzip the file
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_path)
+
+    # Construct the path to the .foam file
+    foam_file = extract_path / "test_2Regions/pv.foam"
+
+    # read the .foam file
+    my_of_reader = OpenFOAMReader(filename=str(foam_file), cell_type=12)
+
+    with pytest.raises(
+        ValueError,
+        match="Subdomain None not found in the OpenFOAM file\. Available subdomains: \['defaultRegion', 'fluid', 'solid']",
+    ):
+        my_of_reader._read_with_pyvista(t=20.0, subdomain=None)
