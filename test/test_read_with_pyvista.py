b67f82f9a1c2e84016262b09ebadf7fa358d1fdd
jhdark <darkj385@mit.edu>
2025-05-05 17:14:05 -0400

    additional test

--- test/test_read_with_pyvista.py
+++ test/test_read_with_pyvista.py
@@ -47,3 +47,23 @@ def test_error_rasied_when_subdomain_is_not_given_in_multidomain_case(tmpdir):
         match="Subdomain None not found in the OpenFOAM file\. Available subdomains: \['defaultRegion', 'fluid', 'solid']",
     ):
         my_of_reader._read_with_pyvista(t=20.0, subdomain=None)
+
+
+@pytest.mark.parametrize("subdomain", ["fluid", "solid"])
+def test_read_with_pyvista_finds_all_mesh_data(tmpdir, subdomain):
+    zip_path = Path("test/data/test_2Regions.zip")
+    extract_path = Path(tmpdir) / "test_2Regions"
+
+    # Unzip the file
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_path)
+
+    # Construct the path to the .foam file
+    foam_file = extract_path / "test_2Regions/pv.foam"
+
+    # read the .foam file
+    my_of_reader = OpenFOAMReader(filename=str(foam_file), cell_type=12)
+
+    my_of_reader._read_with_pyvista(t=20.0, subdomain=subdomain)
+
+    assert subdomain in my_of_reader.OF_meshes_dict
